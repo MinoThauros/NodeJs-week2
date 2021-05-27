c221a70a0d0f1616638d1dd280b22bee05c0625b
MinoThauros <hrako082@uottawa.ca>
2021-05-27 14:06:14 -0400

    Node Mongo DB Example 1

---
+++ index.js
@@ -0,0 +1,45 @@
+const MongoClient = require('mongodb').MongoClient;
+//creates a new instance
+const assert= require('assert');
+
+
+const url= 'mongodb://localhost:27017/';//from the started mongoDB
+const dbname= 'conFusion';//use showdb in  mongo do see a list of all databases
+
+MongoClient.connect(url,(err, client)=>{
+
+    assert.strictEqual(err,null);//perfom a check; that error really is equal to null
+
+    console.log('Connected correctly to the server');
+
+    const db=client.db(dbname);//connect to the desired database
+    const collection = db.collection('dishes');//connect to the desired collection
+    //use the commands use <databasename> and then show collection
+    //connect to a certain database then show the collections inside
+    //a nosql database is made out of documents->collections
+
+    collection.insertOne({
+        "name":"Herimino",
+        "Description":"Test"
+    }, (err,result)=>{//nested callback
+        assert.strictEqual(err,null);
+
+        console.log('After insert: \n')
+        console.log(result.ops);//will show wat was instered
+
+        collection.find({}).toArray((err,docs)=>{
+            assert.strictEqual(err, null);
+            console.log('Found:\n');
+            console.log(docs);//a callback function directly manipulates a function's input
+
+            db.dropCollection('dishes', (err, result)=>{
+                assert.strictEqual(err, null);
+                client.close();
+            })
+        });//find({}) would return everything as quiery conditions weren't specified
+        //would put everything into a dictionary-like array
+    });
+
+
+});
+
