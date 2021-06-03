f15de99ed0f9c96bbd1cea9673779b88bb2ccc27
MinoThauros <hrako082@uottawa.ca>
2021-06-03 14:54:17 -0400

    using MongoDb and callbacks

--- index.js
+++ index.js
@@ -8,38 +8,48 @@ const dboper= require('./operations');
 const url= 'mongodb://localhost:27017/';//from the started mongoDB
 const dbname= 'conFusion';//use showdb in  mongo do see a list of all databases
 
-MongoClient.connect(url,(err, client)=>{
-
-    assert.strictEqual(err,null);//perfom a check; that error really is equal to null
-
+MongoClient.connect(url)
+.then((client)=>{
     console.log('Connected correctly to the server');
-
     const db=client.db(dbname);//connect to the desired database
-
     const document1={
         "name":"Vadonut",
         "description":'Test_Document 1'
     };
-
-    const collection= 'dishes'
-
+    const document2={
+        "name":"Vadonut",
+        "description":'updated Test_Document'
+    };
+    const collection= "dishes";
 
     dboper.insertDocument(db, {
         "name":"Vadonut",
-        "description":'Test_Document 1'
-    }, collection, (result)=>{
+        "description":'Test_Document 1'}, collection)//we built it so that it returns the result of the operation
+    .then((result)=>{
         //implementation of the callback
-        console.log('Instert Document: \n' + result.ops)
-    });
-
-    dboper.findDocuments(db,collection, (result)=>{
+        console.log('Instert Document: \n' + result.ops);
+        return dboper.findDocuments(db,collection)})
+    .then((result)=>{
         console.log(`Found Documents: \n ${result} `);
         console.log(result);//returns all the content of the database
-    
-    });
+        return dboper.updateDocument(db, {name:"Vadonut"}, {description: "updated Test twice"}, "dishes")})
+    .then((response)=>{// no need to specify the entire document
+        console.log("Updated documents: \n", response.result);//search criteria is one propriety
+        //.result returns the numbers of successful operations
+        return dboper.findDocuments(db, collection)
+    .then((docs)=>{
+        console.log("Found updated documents:\n", docs);
+        client.close();//client gotta be closed
+        //callback methods cause increased complexity; it becomes increasingly difficult to analyze the code
+        //Required in order to perform sequencial functions
+                });
+            });   
+        })
+.catch((err=>{
+    console.log(err);
+}))
 
-
-
-
-});
+//the .then design pattern allows to handle the return of previous function
+//effectively allowing sequencial algorithms
+//but preventing the callback hell
 
--- operations.js
+++ operations.js
@@ -7,48 +7,38 @@ const assert= require('assert');
 
 
 /**
- * Inster a desired document in a collection
+ * Inster a desired document in a collection; feeds the .insertOne() function
  * @param {*} db : to specify the database
  * @param {*} document : inster a particular document in a certain collectin
  * @param {*} collection :name of the desire collectuib
  * @param {*} callback :returns either an error or the instered document
  */
-exports.insertDocument=(db, document, collection, callback)=>{
+exports.insertDocument=(db, document, collection)=>{
     const coll=db.collection(collection);
     //the document will be plugged in by the main module
-    coll.insertOne(document, (err, result)=>{
-        assert.strictEqual(err, null);
-        console.log(`Inserted ${result.result.n} documents into the collection` );
-        callback(result);//gives the other module the freedom to manipulate the result
-        //by assigning any fucntion as input we pass the result to that function
-    });
-
-};
+    //coll.insertOne(document, (err, result)=>{
+        //assert.strictEqual(err, null);
+        //console.log(`Inserted ${result.result.n} documents into the collection` );
+        //callback(result);//gives the other module the freedom to manipulate the result
+        //-by assigning any function as input we pass the result to that function
+    return coll.insertOne(document);
+    //we deleted the callback functions because we already know these function will return a promise
+    // promises  allow us to handle the return of a function without using callbacks
+    };
 
 exports.findDocuments=(db, collection, callback)=>{
     const coll=db.collection(collection);
-    coll.find({}).toArray((err,docs)=>{
-        assert.strictEqual(err,null);
-        console.log('Found some documents');
-        callback(docs)
-    })
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument=(db, document, collection, callback)=>{
     const coll=db.collection(collection);
-    coll.deleteOne(document, (err, result)=>{
-        assert.strictEqual(err, null);
-        console.log(`removed the document ${document}`)
-        callback(result)
-    })
+    return  coll.deleteOne(document);
 };
 
-exports.updateDocument=(db, document, update, callback)=>{
+exports.updateDocument=(db, document, update, collection, callback)=>{
     const coll=db.collection(collection);
-    coll.updateOne(document,{$set: update}, null, (err, result)=>{
-        assert.strictEqual(err, null);
-        console.log("updated the document with" + update);
-        callback(result);
+    return coll.updateOne(document,{$set: update}, null)
         
-    } )
+    
 };
